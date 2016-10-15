46cd920cd4195937cd188710a5c6b1121ac003ca
brittneychin <b.chin96@live.com>
2016-10-14 21:42:36 -0500

    Exercise 3 complete

--- maze.js
+++ maze.js
@@ -1,13 +1,38 @@
+var gameLost=false;
+
 window.onload = function(){
 	var walls = document.querySelectorAll(".boundary");
+	var mazeStart = document.getElementById("start");
+	var mazeEnd = document.getElementById("end");
+
+	mazeStart.onmouseover= function(){gameStart(walls)};
+	mazeEnd.onmouseover= function(){gameOver()};
+
 	for (var i = 0; i < walls.length; i++) {
-		walls[i].addEventListener("mouseover",function(){changeColour(walls)},false);
+		walls[i].addEventListener("mouseover",function(){changeColour(walls)});
 
 	}
 };
 
+function gameStart(elem){
+	gameLost=false;
+	for (var i = 0; i <= elem.length; i++) {
+		elem[i].removeClassName("youlose");
+	}
+}
+
 function changeColour(elem){
-	for (var i = elem.length - 1; i >= 0; i--) {
+	gameLost=true;
+	for (var i = 0; i <= elem.length; i++) {
 		elem[i].setAttribute("class","boundary youlose");
+	}
+}
+
+function gameOver(){
+	if(gameLost==false){
+		alert('You Win!');
+	}
+	else{
+		alert('Opps. Game Over!');
 	}
 }
